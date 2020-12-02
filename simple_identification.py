a9c9929bffdb3f0eee751d73dae6513d176a2187
LeSasse <leonard.sasse@hhu.de>
2020-12-02 10:38:35 +0100

    fix

--- simple_identification.py
+++ simple_identification.py
@@ -7,6 +7,8 @@ Created on Tue Dec  1 19:21:37 2020
 """
 
 from scipy.io import loadmat
+import pandas as pd
+
 
 ## data is stored in variable 'res', consists of 399 participants, it has the
 ## following columns:
@@ -17,7 +19,12 @@ from scipy.io import loadmat
 ## falff == fractional Amplitude of Low Frequency Fluctuations,
 ## reho == regional homogeneity
 
-D = loadmat("/home/leonard/projects/master_project_files/dotmat_data/hcp_conn_unrelated_FULLHCP1LRFIX_dos160_roi_atlas_2x2x2.mat")
+D = loadmat(
+    "/home/leonard/projects/master_project_files/dotmat_data/hcp_conn_unrelated_FULLHCP1LRFIX_dos160_roi_atlas_2x2x2.mat",
+    squeeze_me = False,
+    mat_dtype = True,
+    struct_as_record = True,
+    simplify_cells = True)
 
 
 ## defining the variables
@@ -30,8 +37,19 @@ sub = D['res']['sub']
 
 
 
-print(connm.ndim)
-print(connm.shape)
-print(connm.size)
-print(connm.dtype)
+#print(connm.ndim)
+#print(connm.shape)
+#print(connm.size)
+#print(connm.dtype)
 
+#print(connm)
+
+connectivity_data = pd.DataFrame(connm, sub)
+re_homogeneity = pd.DataFrame(reho, sub)
+df_alff = pd.DataFrame(alff, sub)
+df_falff = pd.DataFrame(falff, sub)
+
+#print(connectivity_data)
+#print(re_homogeneity)
+#print(df_alff)
+#print(df_falff)
