fixed creation of gradient database, this was the reason, that identification was not working

using_pearson_correlation/alignment_method_one.py
@@ -10,7 +10,7 @@ Created on Sat Dec 19 19:25:20 2020
 
 import pandas as pd
 from brainspace.gradient import GradientMaps
-import numpy as np
+
 
 ## my own imports
 import load_data_and_functions as ldf
@@ -24,7 +24,6 @@ kernel = "pearson"
 dimension_reduction = "dm"
 alignment = "procrustes"
 atlas_size = 160
-
 
 ## Loading Data ##############################################################
 D, D_sub, D_connectivity_data, D_cd_transposed = ldf.load_data(database_path)
@@ -41,25 +40,16 @@ gref = GradientMaps(n_components=1, kernel = kernel,
 gref.fit(reference_participant)
 
 ### Database Gradient Construction ###########################################
-gradient_array_database = np.zeros((len(D_sub), atlas_size))
-for index, subject in enumerate(D_sub):
-    subject_matrix = ldf.get_conn_matrix(D_cd_transposed[subject])
-                                         
-    database_gm = GradientMaps(n_components=1,
-                               kernel = kernel,
-                               approach = dimension_reduction,
-                               random_state=0,
-                               alignment = alignment)
+gradient_dataframe_transposed = ldf.create_gradient_database(
+    dataframe = D_cd_transposed,
+    subjects = D_sub, 
+    atlas_size = atlas_size, 
+    reference = gref.gradients_, 
+    kernel = kernel,
+    dimension_reduction = dimension_reduction, 
+    alignment = alignment
+        )
     
-    database_gm.fit(subject_matrix, reference = gref.gradients_)
-    gradient_array_database[index] = database_gm.aligned_[:,0]
-
-## So now we have a database consisting of gradients from all participants in 
-## database D
-gradient_dataframe = pd.DataFrame(gradient_array_database)
-gradient_dataframe_transposed = gradient_dataframe.T
-gradient_dataframe_transposed.columns = list(D_sub)
-
 ### Identification Analysis ##################################################
 ### Target Gradients and Identification from Database
 
@@ -78,8 +68,9 @@ for index, subject in enumerate(Y_sub):
     subject_gradient = gm.aligned_[:,0]
     subject_gradient_dataframe = pd.DataFrame(subject_gradient)
     
-    all_corr = gradient_dataframe_transposed.corrwith(subject_gradient_dataframe.iloc[:,0],
-                                                      method = 'pearson')
+    all_corr = gradient_dataframe_transposed.corrwith(
+        subject_gradient_dataframe.iloc[:,0], 
+        method = 'pearson')
     max_value = max(all_corr)
     max_index = all_corr.index[all_corr == max_value]
     

using_pearson_correlation/alignment_method_two.py
@@ -10,7 +10,7 @@ Created on Sun Dec 20 12:37:48 2020
 
 import pandas as pd
 from brainspace.gradient import GradientMaps
-import numpy as np
+
 
 ## my own imports
 import load_data_and_functions as ldf
@@ -46,23 +46,20 @@ for index, subject in enumerate(Y_sub):
     gm.fit(subject_connm)
     subject_gradient = gm.gradients_[:,0]
     subject_gradient_dataframe = pd.DataFrame(subject_gradient)
-    gradient_array_database = np.zeros((len(D_sub), atlas_size))
+    
     
     ### Database Gradient Construction #######################################
     ### This will make sure that the gradients from database are always aligned
     ### to the target gradient to be identified
-    for index2, subject2 in enumerate(D_sub):
-        subject_matrix = ldf.get_conn_matrix(D_cd_transposed[subject2])
-    
-        database_gm = GradientMaps(n_components=1, kernel = kernel, 
-                                   approach = dimension_reduction,
-                                   random_state=0, alignment = alignment)
-        database_gm.fit(subject_matrix, reference = gm.gradients_)
-    
-        gradient_array_database[index2] = database_gm.aligned_[:,0]
-        gradient_dataframe = pd.DataFrame(gradient_array_database)
-        gradient_dataframe_transposed = gradient_dataframe.T
-        gradient_dataframe_transposed.columns = list(D_sub)
+    gradient_dataframe_transposed = ldf.create_gradient_database(
+        dataframe = D_cd_transposed,
+        subjects = D_sub, 
+        atlas_size = atlas_size, 
+        reference = gm.gradients_, 
+        kernel = kernel,
+        dimension_reduction = dimension_reduction, 
+        alignment = alignment
+        )
     
     all_corr = gradient_dataframe_transposed.corrwith(subject_gradient_dataframe.iloc[:,0],
                                                       method = 'pearson')

using_pearson_correlation/gradient_identification.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 from brainspace.gradient import GradientMaps
-import numpy as np
+
 
 ## my own imports
 import load_data_and_functions as ldf
@@ -17,42 +17,21 @@ dimension_reduction = "dm"
 alignment = "procrustes"
 atlas_size = 160
 
-
 ## Loading Data ##############################################################
 D, D_sub, D_connectivity_data, D_cd_transposed = ldf.load_data(database_path)
 Y, Y_sub, Y_connectivity_data, Y_cd_transposed = ldf.load_data(target_path)
 
-
 ### Gradient Construction ####################################################
-
 ## So far gradients are NOT aligned in this code
-## align gradients using procrustes alignment (there will be two sep-
-## implementations to investigate here: either all gradients will be aligned to
-## one gradient or the gradient to be identified will be used to align all other
-## gradients in each iteration) 
-
-
 ### Database Gradient Construction ###########################################
-gradient_array_database = np.zeros((len(D_sub), atlas_size))
-for index, subject in enumerate(D_sub):
-    subject_connm = ldf.get_conn_matrix(D_cd_transposed[subject])
-    
-    gm = GradientMaps(n_components=1,
-                      kernel = kernel,
-                      approach = dimension_reduction,
-                      random_state=0)
-    gm.fit(subject_connm)
-    
-    
-    gradient_array_database[index] = gm.gradients_[:,0]
-
-## So now we have a database consisting of gradients from all participants in 
-## database D
-### IMPORTANT: these gradients are not aligned
-gradient_dataframe = pd.DataFrame(gradient_array_database)
-gradient_dataframe_transposed = gradient_dataframe.T
-gradient_dataframe_transposed.columns = list(D_sub)
-
+gradient_dataframe_transposed = ldf.create_gradient_database(
+    dataframe = D_cd_transposed,
+    subjects = D_sub, 
+    atlas_size = atlas_size,  
+    kernel = kernel,
+    dimension_reduction = dimension_reduction, 
+    alignment = alignment
+        )
 
 ### Identification Analysis ##################################################
 ### Target Gradients and Identification from Database
@@ -87,6 +66,4 @@ rate = count1/len(Y_cd_transposed.columns)
 print(str(rate*100) + '% of subjects in were accurately predicted from data in '
       'dataset D.')
     
-    
-
 