9a997ba8719cd1a4fb84eb2e5cc537db0cc8136f
LeSasse <leonard.sasse@hhu.de>
2020-12-31 07:22:25 +0100

    formatting

--- using_pearson_correlation/alignment_method_one.py
+++ using_pearson_correlation/alignment_method_one.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Dec 19 19:25:20 2020
-
 @author: leonard
 """
 
@@ -17,8 +16,12 @@ import load_data_and_functions as ldf
 
 ### Global Variables/User input ##############################################
 
-database_path = '../../dotmat_data/hcp_conn_unrelated_FULLHCP1LRFIX_dos160_roi_atlas_2x2x2.mat'
-target_path = '../../dotmat_data/hcp_conn_unrelated_FULLHCP2LRFIX_dos160_roi_atlas_2x2x2.mat'
+database_path = (
+    "../../dotmat_data/hcp_conn_unrelated_FULLHCP1LRFIX_dos160_roi_atlas_2x2x2.mat"
+)
+target_path = (
+    "../../dotmat_data/hcp_conn_unrelated_FULLHCP2LRFIX_dos160_roi_atlas_2x2x2.mat"
+)
 
 kernel = "pearson"
 dimension_reduction = "dm"
@@ -33,52 +36,56 @@ Y, Y_sub, Y_connectivity_data, Y_cd_transposed = ldf.load_data(target_path)
 ### Gradient Construction ####################################################
 ## Reference Gradient for Alignment
 ## In alignment method one I will align all gradients to one reference gradient.
-reference_participant = ldf.get_conn_matrix(D_cd_transposed.iloc[:,0])   
-gref = GradientMaps(n_components=1, kernel = kernel, 
-                    approach = dimension_reduction,
-                    random_state=0)
+reference_participant = ldf.get_conn_matrix(D_cd_transposed.iloc[:, 0])
+gref = GradientMaps(
+    n_components=1, kernel=kernel, approach=dimension_reduction, random_state=0
+)
 gref.fit(reference_participant)
 
 ### Database Gradient Construction ###########################################
 gradient_dataframe_transposed = ldf.create_gradient_database(
-    dataframe = D_cd_transposed,
-    subjects = D_sub, 
-    atlas_size = atlas_size, 
-    reference = gref.gradients_, 
-    kernel = kernel,
-    dimension_reduction = dimension_reduction, 
-    alignment = alignment
-        )
-    
+    dataframe=D_cd_transposed,
+    subjects=D_sub,
+    atlas_size=atlas_size,
+    reference=gref.gradients_,
+    kernel=kernel,
+    dimension_reduction=dimension_reduction,
+    alignment=alignment,
+)
+
 ### Identification Analysis ##################################################
 ### Target Gradients and Identification from Database
 
 count1 = 0
 for index, subject in enumerate(Y_sub):
-    
+
     subject_connm = ldf.get_conn_matrix(Y_cd_transposed[subject])
     ## gradient construction using the matrices for each subject to be "identi-
     ## fied" in the "target set" Y
-    gm = GradientMaps(n_components=1,
-                      kernel = kernel,
-                      approach = dimension_reduction,
-                      random_state=0,
-                      alignment = alignment)
-    gm.fit(subject_connm, reference = gref.gradients_)
-    subject_gradient = gm.aligned_[:,0]
+    gm = GradientMaps(
+        n_components=1,
+        kernel=kernel,
+        approach=dimension_reduction,
+        random_state=0,
+        alignment=alignment,
+    )
+    gm.fit(subject_connm, reference=gref.gradients_)
+    subject_gradient = gm.aligned_[:, 0]
     subject_gradient_dataframe = pd.DataFrame(subject_gradient)
-    
+
     all_corr = gradient_dataframe_transposed.corrwith(
-        subject_gradient_dataframe.iloc[:,0], 
-        method = 'pearson')
+        subject_gradient_dataframe.iloc[:, 0], method="pearson"
+    )
     max_value = max(all_corr)
     max_index = all_corr.index[all_corr == max_value]
-    
-    if max_index[0] == subject:
-        count1 = count1 +1
-        #print(subject) to know who was correctly identified
-    
-rate = count1/len(Y_cd_transposed.columns)
 
-print(str(rate*100) + '% of subjects in were accurately predicted from data in '
-      'dataset D.')
+    if max_index[0] == subject:
+        count1 = count1 + 1
+        # print(subject) to know who was correctly identified
+
+rate = count1 / len(Y_cd_transposed.columns)
+
+print(
+    str(rate * 100) + "% of subjects in were accurately predicted from data in "
+    "dataset D."
+)
--- using_pearson_correlation/load_data_and_functions.py
+++ using_pearson_correlation/load_data_and_functions.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Dec 30 09:13:29 2020
-
 @author: leonard
 
 Loading Data 
@@ -24,82 +23,74 @@ import numpy as np
 import math
 from brainspace.gradient import GradientMaps
 
+
 def load_data(data_path):
-    '''input is path that leads to data as a string,
-    Output is D = the whole datafile, D_sub = series of subject id numbers, 
+    """input is path that leads to data as a string,
+    Output is D = the whole datafile, D_sub = series of subject id numbers,
     D_connectivity_data is the subjects concatenated connectivity data, last out-
-    put is transposed connectivity data'''
+    put is transposed connectivity data"""
     D = loadmat(
         data_path,
-        squeeze_me = False,
-        mat_dtype = True,
-        struct_as_record = True,
-        simplify_cells = True)
-    
-    D_connm = D['res']['connm']
-    D_sub = D['res']['sub']
+        squeeze_me=False,
+        mat_dtype=True,
+        struct_as_record=True,
+        simplify_cells=True,
+    )
+
+    D_connm = D["res"]["connm"]
+    D_sub = D["res"]["sub"]
     D_connectivity_data = pd.DataFrame(D_connm, D_sub)
     D_cd_transposed = D_connectivity_data.transpose()
-    
+
     return D, D_sub, D_connectivity_data, D_cd_transposed
 
-def get_conn_matrix( X ):
-    '''function for obtaining participants connectivity matrix input should be 
-    a pandas series containing the connectivity data for one subject'''
+
+def get_conn_matrix(X):
+    """function for obtaining participants connectivity matrix; input should be
+    a pandas series containing the connectivity data for one subject"""
     n = len(X)
-    N = round((math.sqrt(8*n+1)+1)/2)
-    square_zeros = np.zeros((N,N))
-    indices = np.nonzero(np.triu(np.ones((N,N)), 1))
+    N = round((math.sqrt(8 * n + 1) + 1) / 2)
+    square_zeros = np.zeros((N, N))
+    indices = np.nonzero(np.triu(np.ones((N, N)), 1))
     square_zeros[indices] = X
     square_connm = square_zeros + np.transpose(square_zeros)
     return np.eye(N) + square_connm
 
-def create_gradient_database(dataframe,
-                             subjects, 
-                             atlas_size, 
-                             reference = 0, 
-                             kernel = 'pearson',
-                             dimension_reduction = 'dm', 
-                             alignment = 'procrustes'):
-    ''' Input transposed connectivity data to construct subjects gradient, list
-    of subjects, size of the atlas and a reference gradient to use for 
-    alignment (default is none). For gradient construction pearson kernel and 
-    dm approach are default'''
-    gradient_array_database = np.zeros((len(subjects), atlas_size)) 
-    
-    if np.all(reference == 0):
-        for index, subject in enumerate(subjects):
-            subject_matrix = get_conn_matrix(dataframe[subject])
-            
-            database_gm = GradientMaps(n_components=1, 
-                                   kernel = kernel, 
-                                   approach = dimension_reduction,
-                                   random_state=0,
-                                   alignment = alignment)
-        
+
+def create_gradient_database(
+    dataframe,
+    subjects,
+    atlas_size,
+    reference=None,
+    kernel="pearson",
+    dimension_reduction="dm",
+    alignment="procrustes",
+):
+    """Input transposed connectivity data to construct subjects gradient, list
+    of subjects, size of the atlas and a reference gradient to use for
+    alignment (default is none). For gradient construction pearson kernel and
+    dm approach are default"""
+    gradient_array_database = np.zeros((len(subjects), atlas_size))
+
+    for index, subject in enumerate(subjects):
+        subject_matrix = get_conn_matrix(dataframe[subject])
+
+        database_gm = GradientMaps(
+            n_components=1,
+            kernel=kernel,
+            approach=dimension_reduction,
+            random_state=0,
+            alignment=alignment,
+        )
+
+        if np.all(reference == None):
             database_gm.fit(subject_matrix)
-            gradient_array_database[index] = database_gm.gradients_[:,0]
-            
-        gradient_dataframe = pd.DataFrame(gradient_array_database)
-        gradient_dataframe_transposed = gradient_dataframe.T
-        gradient_dataframe_transposed.columns = list(subjects)
-    else:
-        for index, subject in enumerate(subjects):
-            subject_matrix = get_conn_matrix(dataframe[subject])
-    
-            database_gm = GradientMaps(n_components=1, 
-                                   kernel = kernel, 
-                                   approach = dimension_reduction,
-                                   random_state=0,
-                                   alignment = alignment)
-        
-            database_gm.fit(subject_matrix, reference = reference)
-            gradient_array_database[index] = database_gm.aligned_[:,0]
-            
-        gradient_dataframe = pd.DataFrame(gradient_array_database)
-        gradient_dataframe_transposed = gradient_dataframe.T
-        gradient_dataframe_transposed.columns = list(subjects)
-        
+            gradient_array_database[index] = database_gm.gradients_[:, 0]
+        else:
+            database_gm.fit(subject_matrix, reference=reference)
+            gradient_array_database[index] = database_gm.aligned_[:, 0]
+
+    gradient_dataframe = pd.DataFrame(gradient_array_database)
+    gradient_dataframe_transposed = gradient_dataframe.T
+    gradient_dataframe_transposed.columns = list(subjects)
     return gradient_dataframe_transposed
-
-
