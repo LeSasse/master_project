36dd1d50665a274f037f4939f02fd0cf64c157b3
LeSasse <leonard.sasse@hhu.de>
2021-02-22 20:18:32 +0100

    worked on cma-es, not sure if objective function is doing the correct thing

--- Identification/controls_metric_learn_cma.py
+++ Identification/controls_metric_learn_cma.py
@@ -11,8 +11,6 @@ import sys
 import pickle
 import pandas as pd
 import numpy as np
-import pdb
-import random
 from datetime import datetime
 import cma
 from sklearn.neighbors import NearestNeighbors
@@ -60,11 +58,11 @@ session2_path = (
 ##############################################################################
 
 
-RUNS = 10
+RUNS = 2
 MIN, MAX = -1.0, 1.0
 MAXFEVALS = 500
 TRAINPROP = 0.5
-
+SAVE = True
 
 ## Making sure that both dataframes have same participants ###################
 for col in session1_cd_transposed.columns:
@@ -77,72 +75,48 @@ for col in session2_cd_transposed.columns:
 ##############################################################################
 
 
-'''
-## Identification ############################################################
-##############################################################################
-rate1 = ldf.identify(target=session2_cd_transposed, database=session1_cd_transposed)
-rate2 = ldf.identify(target=session1_cd_transposed, database=session2_cd_transposed)
-
-rate = (rate1 + rate2) / 2
-'''
-
-## initial random weights ####################################################
-'''
-vector w for weights
-matrix A for session1 
-matrix B for session2
-'''
-
-
 ### summing data ... ##########################################
 control1 = ldf.create_control_data(session1_cd_transposed, kind="sum", atlas_size=160)
 control2 = ldf.create_control_data(session2_cd_transposed, kind="sum", atlas_size=160)
 
+control1_T = control1.T
+control2_T = control2.T
+
+n = control1.shape[0]   #398
+d = control1.shape[1]   #160
 
 
-n = control1.shape[0]   #160
-d = control1.shape[1]   #398
-
-## generating some random weights to start with ... 
-w1 = np.random.rand(int(n/2),)
-w2 = np.random.rand(int(n/2),) * -1
-w = np.append(w1, w2)
-w = np.random.permutation(w)
-
-## ... then weighting session 1
-A = np.array(control1)
-Aw = (A.T * w).T
-
-## ... then weighting session 2
-B = np.array(control2)
-Bw = (B.T * w).T
-
-## identification with randomly generated initial weights
-data1 = pd.DataFrame(Aw)
-data2 = pd.DataFrame(Bw)
-N = data1.shape[0]
-D = data1.shape[1]  
-data1_ranked = data1.T.rank()
-data2_ranked = data2.T.rank()
-nbrs = NearestNeighbors(n_neighbors=1, metric='correlation', n_jobs= 1).fit(data1_ranked)
-distances, indices = nbrs.kneighbors(data2_ranked)
-rate1 = np.mean(indices.T == np.array(list(range(D))))
-nbrs = NearestNeighbors(n_neighbors=1, metric='correlation', n_jobs= 1).fit(data2_ranked)
-distances, indices = nbrs.kneighbors(data1_ranked)
-rate2 = np.mean(indices.T == np.array(list(range(D))))
-rate = (rate1 + rate2)/2
+### Identification without weights
+rate1 = ldf.identify(control1, control2)
+rate2 = ldf.identify(control2, control1)
+rate = (rate1 + rate2) / 2
 
 
-'''
 ## cma options 
 cmaopts = cma.CMAOptions()
-cmaopts.set('bounds', [[MIN]*d, [MAX]*D])
+cmaopts.set('bounds', [[MIN]*n, [MAX]*n])
 cmaopts.set('maxfevals', MAXFEVALS)
-'''
 
-def objectiveFUN(w, data1, data2):
+
+
+# from: https://stackoverflow.com/questions/38641691/weighted-correlation-coefficient-with-pandas
+def mw(x, w):
+    """Weighted Mean"""
+    return np.sum(x * w) / np.sum(w)
+
+def covw(x, y, w):
+    """Weighted Covariance"""
+    return np.sum(w * (x - mw(x, w)) * (y - mw(y, w))) / np.sum(w)
+
+def corrw(x, y, w):
+    """Weighted Correlation"""
+    return covw(x, y, w) / np.sqrt(covw(x, x, w) * covw(y, y, w))
+
+def eval_id(w, data1, data2):
+    ''' Evaluate weighted Identification ''' 
     A = np.array(data1)
     B = np.array(data2)
+    w = np.array(w)
     
     ## weighting columns
     Aw = (A.T * w).T
@@ -154,27 +128,220 @@ def objectiveFUN(w, data1, data2):
     N = data1.shape[0]
     D = data1.shape[1]             
     
-    ### identification method ###
-    '''
-    Depending on how tests on gradients go, I will have to implement the ident-
-    fication method using pandas.corrwith():
-    However, ranking data and then using pearson correlation, should be equiva-
-    lent to spearman correlation on original data. Nonetheless identification 
-    results are slightly different (+-1 or 2 percentage points), will have to 
-    see how much it affects results overall
-    '''
-    
-    data1_ranked = data1.T.rank()
-    data2_ranked = data2.T.rank()
-    
-    ## first way
+    data1_ranked = data1.rank().T
+    data2_ranked = data2.rank().T
+
     nbrs = NearestNeighbors(n_neighbors=1, metric='correlation', n_jobs= 1).fit(data1_ranked)
     distances, indices = nbrs.kneighbors(data2_ranked)
     rate1 = np.mean(indices.T == np.array(list(range(D))))
-    
+        
     nbrs = NearestNeighbors(n_neighbors=1, metric='correlation', n_jobs= 1).fit(data2_ranked)
     distances, indices = nbrs.kneighbors(data1_ranked)
     rate2 = np.mean(indices.T == np.array(list(range(D))))
     
-    rate = (rate1 + rate2)/2
-    return rate
+    obj = (rate1 + rate2)/2
+    
+    return obj
+
+
+def objectiveFUN(w, data1, data2):
+    
+    data1= data1.reset_index(drop=True)
+    data2= data2.reset_index(drop=True)
+    data1= data1.rank()
+    data2= data2.rank()
+    w = np.array(w)
+    
+    ## initialise ##
+    crrsii = np.array([])
+    crrsij = np.array([]) 
+    
+    for index, row in data1.iterrows():
+        crrmax = -np.inf
+        mycrrs = np.array([])
+        
+        x1 = row.to_numpy()
+        y1 = data2.iloc[index].to_numpy()
+        for index2, row2 in data2.iterrows():
+            y2 = row2.to_numpy()
+            x2 = data1.iloc[index2].to_numpy()
+            if index == index2:
+                crrself = corrw(x1, y1, w)
+                crrsii = np.append(crrsii, crrself)
+            else:
+                mycrrs = np.append(mycrrs, corrw(x1, x2, w))
+                mycrrs = np.append(mycrrs, corrw(x1, y2, w))
+                mycrrs = np.append(mycrrs, corrw(y1, x2, w))
+                mycrrs = np.append(mycrrs, corrw(y1, y2, w))
+                crrsij = np.append(crrsij, mycrrs)
+                crr = np.max(mycrrs)
+                
+                if crr > crrmax:
+                    crrmax = crr
+                    
+    obj = -(np.mean(crrsii) - np.mean(crrsij))
+    return obj
+
+def objective_fun(w, data1, data2):
+   data1 = data1.T
+   data2 = data2.T
+   w = np.array(w)
+   A = np.array(data1)
+   B = np.array(data2)
+    
+   ## weighting columns
+   Aw = (A.T * w).T
+   Bw = (B.T * w).T
+   #return pd.DataFrame(Aw)
+    
+   data1 = pd.DataFrame(Aw)
+   data2 = pd.DataFrame(Bw)
+   N = data1.shape[0]
+   D = data1.shape[1]
+   
+   data1_ranked = data1.T.rank()
+   data2_ranked = data2.T.rank()
+   w = np.array(w)
+   
+   crrsii = np.array([])
+   crrsij = np.array([])
+   
+   for index, row in data1_ranked.iterrows():
+       crrmax = -np.inf
+       mycrrs = np.array([])
+       x1 = row.to_numpy()
+       y1 = data2_ranked.iloc[index].to_numpy()
+       for index2, row2 in data2.iterrows():
+           y2 = row2.to_numpy()
+           x2 = data1_ranked.iloc[index2].to_numpy()
+           
+           if index == index2:
+               crrself = np.correlate(x1, y1)
+               crrsii = np.append(crrsii, crrself)
+               
+           else:
+               mycrrs = np.append(mycrrs, np.correlate(x1, x2))
+               mycrrs = np.append(mycrrs, np.correlate(x1, y2))
+               mycrrs = np.append(mycrrs, np.correlate(y1, x2))
+               mycrrs = np.append(mycrrs, np.correlate(y1, y2))
+               crrsij = np.append(crrsij, mycrrs)
+               crr = np.max(mycrrs)
+               
+               if crr > crrmax:
+                   crrmax = crr
+                   
+   obj = -(np.mean(crrsii) - np.mean(crrsij)) 
+                   
+   return obj
+
+
+### generating some random weights
+weights = [1.0/n]*n
+#defidiff = objectiveFUN(weights, control1_T, control2_T)
+defaccuracy = eval_id(weights, control1, control2)
+
+
+#weights += np.random.uniform(0.0, 0.1/n, n )
+
+
+
+### display initial settings
+print("Initial Settings")
+print("-----------------------------------------------------------")
+print("Identification without weights:            {}.".format(round(rate,4)))
+print("Identification with starting weights:      {}.".format(round(defaccuracy,4)))
+#print("idiff with starting weights:               {}.".format(round(defidiff,4)))
+
+print("-----------------------------------------------------------")
+print("Datasets:")
+print(control1_T)
+print(control2_T)
+
+
+results = {}
+
+seed = np.random.choice(99999, 1)[0]
+print("seed: " + str(seed))
+np.random.seed(seed)
+results['seed'] = seed
+bests = np.empty((0, n))
+totaltime = datetime.timestamp(datetime.now())
+### running iterative optimisation
+for run in range(RUNS):
+    starttime = datetime.timestamp(datetime.now())
+    print("starting run " + str(run))
+    
+    ### train-test split
+    trainidx = np.random.rand(len(control1_T)) < TRAINPROP
+    train = control1_T[trainidx]
+    train = train.reset_index(drop=True)
+    test = control1_T[~trainidx]
+    test = test.reset_index(drop=True)
+    train2 = control2_T[trainidx]
+    train2 = train2.reset_index(drop=True)
+    test2 = control2_T[~trainidx]
+    test2 = test2.reset_index(drop=True)
+    
+    
+    trainmar = objective_fun(weights, train, train2)
+    ### train
+    x0 = weights
+    x0 += np.random.uniform(0.0, 0.1/n, n)
+    
+    res = cma.fmin(objective_fun, x0=x0, sigma0=np.mean(x0), options=cmaopts, args=(train, train2), eval_initial_x=True)
+    
+    bestw = np.array(res[0])
+    bests = np.vstack((bests, bestw))
+    bestw *= 10.0
+    
+    
+    ### test
+    testmar = objective_fun(weights, test, test2)
+    testwgtmar = objective_fun(bestw, test, test2)
+    
+    ### store results
+    results[str(run) + "fitness"] = np.float(res[1])
+    results[str(run) + "solution"] = np.array(res[0])
+    results[str(run) + 'trainidx'] = trainidx
+    results[str(run) + 'trainmar'] = trainmar
+    results[str(run) + 'testmar'] = testmar
+    results[str(run) + 'testwgtmar'] = testwgtmar
+
+    ## wrap up run
+    stoptime = datetime.timestamp(datetime.now())
+    runtime = stoptime-starttime
+    avg_runtime = totaltime / run
+    time_left = totaltime - (avg_runtime * RUNS)
+    print("finished run")
+    
+    ### output for this run
+    print("Run: " + str(run) + " out of " + str(RUNS))
+    print("Results")
+    print("-----------------------------------------------------------")
+    print("idiff training data:            {}.".format(trainmar))
+    print("idiff test data:                {}.".format(testmar))
+    print("best function value:            {}.".format(res[1]))
+    print("-----------------------------------------------------------")
+    print("Run Timestamp (in seconds)")
+    print("-----------------------------------------------------------")
+    print("Time for this run:              {}.".format(runtime))
+    print("Avg run time:                   {}.".format(avg_runtime))
+    print("Total time so far:              {}.".format(totaltime))
+    print("Time left                       {}.".format(time_left))
+    print("-----------------------------------------------------------")
+    
+
+results['bestmean'] = np.mean(bests, 0)
+print('average of bests solutions:')
+print("-----------------------------------------------------------")
+print( results['bestmean'] )
+
+if SAVE:
+    now = datetime.now()
+    fname = 'metric_learning_cma_' + now.strftime("%m%d%Y_%H%M%S") + '.pickle'
+    print("saving results in: " + fname)
+    with open(fname,'wb') as f:
+        pickle.dump(results, f)
+
+
+
